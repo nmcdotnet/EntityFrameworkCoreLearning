bf3411dcd1082f00730968fc67a33193f91a4f4a
nmc.net <kysutudongcom@gmail.com>
2024-07-23 20:42:00 +0700

    Update Doc Folder

---
+++ EFCore.Domain/Team.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Domain
+{
+    public class Team :BaseDomainModel
+    {
+        public int TeamId { get; set; } 
+        public string? Name { get; set; }
+    }
+    public class Coach : BaseDomainModel
+    {
+        public int CoachId { get; set; }
+        public string? Name { get; set; }
+    }
+
+    public abstract class BaseDomainModel
+    {
+        public DateTime CreatedDate { get; set; }
+    }
+}
